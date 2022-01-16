3337cf3ae6a8edebebadac28be5f87b89f09c3cf
juliangranda <juliangranda@hotmail.com>
2022-01-16 17:28:23 -0500

    instalacion de react y react-dom

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//snippets: imr, slr
+const App = () => {
+    return (
+        <h1>hola mundo</h1>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+ReactDOM.render(<app/>, document.getElementById('app'));
