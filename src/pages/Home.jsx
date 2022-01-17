0b0cd3d980e749c3354e68ec2e86e0be8b27f37c
juliangranda <juliangranda@hotmail.com>
2022-01-17 17:35:51 -0500

    header en las rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-
+import Header from '../components/Header';
 const Home = () => {
     return (
-        <div>
-            <h1>React shop</h1>
-        </div>
+        <Header/>
     );
 }
 
